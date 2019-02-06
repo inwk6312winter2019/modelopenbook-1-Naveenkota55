28f5eadb7e229d4fb8644c0ca9672de7b7ca09c0
Naveenkota55 <naveen.kota@dal.ca>
2019-02-06 13:07:52 -0400

    2

--- openbook.py
+++ openbook.py
@@ -1,3 +1,27 @@
-fin=open("running-config.cfg","r")
-def list_ifname_ip:
-	
+file=open("running-config.cfg","r")
+term = "nameif"
+term2="ip address"
+def list_ifname_ip():
+        for line in file:
+                line.strip().split('/n')
+                if term in line:
+                        print (line)
+                if term2 in line:
+                        print (line)
+               
+
+list_ifname_ip()
+file.close()
+
+
+
+
+
+
+
+'''
+
+	for lines in fin:
+		lines1=lines.split()
+		print (lines)
+'''
