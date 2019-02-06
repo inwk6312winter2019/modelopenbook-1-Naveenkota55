9a36361e97677faf26104bd757368ef11bfdf6d1
Naveenkota55 <naveen.kota@dal.ca>
2019-02-06 13:15:50 -0400

    3

--- openbook.py
+++ openbook.py
@@ -1,16 +1,22 @@
 file=open("running-config.cfg","r")
 term = "nameif"
 term2="ip address"
+dict={}
 def list_ifname_ip():
         for line in file:
                 line.strip().split('/n')
                 if term in line:
-                        print (line)
+                        line_nameif=line.replace('nameif','')
+                        print (line_nameif)
                 if term2 in line:
-                        print (line)
-               
+                        line_ip_address=line.replace('ip address','')
+                        print (line_ip_address)
+                
+                #dict.update({line_nameif:line_ip_address})
+                
 
 list_ifname_ip()
+print (dict)
 file.close()
 
 
